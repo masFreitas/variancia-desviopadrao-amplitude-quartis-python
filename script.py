3b9fdf7f50cc08965298dc55e5f99f6a918b3f16
masFreitas <masteusasfreitas@gmail.com>
2024-11-18 21:38:08 -0300

    feat: adição de frequencia relativa

--- script.py
+++ script.py
@@ -38,6 +38,13 @@ print(df.describe())
 
 # Exemplo 2:
 # Distribuição de frequência por classes da coluna idade do imóvel (HouseAge)
-classes = pd.cut(df['HouseAge'], bins=[0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100], labels=['0-10','10-20','20-30','30-40','40-50','50-60','60-70','70-80','80-90','90-100'])
-freq_por_classe = classes.value_counts()
-print(freq_por_classe)
+bins = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+labels = ['0-10', '10-20', '20-30', '30-40', '40-50', '50-60', '60-70', '70-80', '80-90', '90-100']
+df['HouseAgeClass'] = pd.cut(df['HouseAge'], bins=bins, labels=labels)
+freq = df['HouseAgeClass'].value_counts()
+total = len(df['HouseAgeClass'])
+freq_rel = freq / total
+print('Distribuição de Frequência por Classes da HouseAge:')
+print(freq)
+print('\nFrequência Relativa por Classes da HouseAge:')
+print(freq_rel)
